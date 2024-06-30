65252c96bbd5a09454de173c52265dd4b7a3ae76
Irina Ichim <onlyirina7@gmail.com>
2024-06-30 03:49:44 +0200

    Add component and complete About Us page

--- src/features/About/page/AboutPage.tsx
+++ src/features/About/page/AboutPage.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import iconMission from '/iconMission.png';
-import iconVision from '/iconVision.png';
-import FemCodersClubLogo from '/FemCodersClubLogo.png';
-import './AboutPage.css';
-import CarouselValues from '../components/CarouselValues';
-import iconCommitment from '/iconCommitment.png';
-import imgCommitment from '/imgCommitment.png';
+import React from "react";
+import iconMission from "/iconMission.png";
+import iconVision from "/iconVision.png";
+import FemCodersClubLogo from "/FemCodersClubLogo.png";
+import "./AboutPage.css";
+import CarouselValues from "../components/CarouselValues";
+import iconCommitment from "/iconCommitment.png";
+import imgCommitment from "/imgCommitment.png";
+import Collapse from "../components/Collapse";
 
 const AboutPage: React.FC = () => {
   return (
@@ -14,17 +15,44 @@ const AboutPage: React.FC = () => {
         <div className="banner-quienes-somos">
           <h1>Comunidad de mujeres desarrolladoras</h1>
           <p>
-            El objetivo principal de <strong>femCoders Club</strong> es cerrar la brecha de género en el ámbito digital y ser un referente que promueva la inclusión femenina en el mundo tecnológico. Somos un grupo de mujeres apasionadas por la tecnología que <strong>trabajamos para fomentar la participación activa de las mujeres en el sector de TI.</strong>
+            El objetivo principal de <strong>femCoders Club</strong> es cerrar
+            la brecha de género en el ámbito digital y ser un referente que
+            promueva la inclusión femenina en el mundo tecnológico. Somos un
+            grupo de mujeres apasionadas por la tecnología que{" "}
+            <strong>
+              trabajamos para fomentar la participación activa de las mujeres en
+              el sector de TI.
+            </strong>
           </p>
         </div>
       </section>
       <section className="parallax-bg1">
         <div className="container-quienes-somos">
           <p className="description text-left">
-            <strong>Nuestro objetivo es crear un espacio seguro donde podamos reunirnos y colaborar para crecer juntas.</strong> Para lograrlo, organizamos actividades que visibilizan a las mujeres programadoras y promovemos su desarrollo profesional. A través de diálogos abiertos y respetuosos, buscamos derribar barreras y estereotipos, empoderando a todas las mujeres para prosperar en sus carreras tecnológicas. Mediante eventos, talleres y recursos, trabajamos unidas para generar un impacto duradero en la industria de la tecnología.
+            <strong>
+              Nuestro objetivo es crear un espacio seguro donde podamos
+              reunirnos y colaborar para crecer juntas.
+            </strong>{" "}
+            Para lograrlo, organizamos actividades que visibilizan a las mujeres
+            programadoras y promovemos su desarrollo profesional. A través de
+            diálogos abiertos y respetuosos, buscamos derribar barreras y
+            estereotipos, empoderando a todas las mujeres para prosperar en sus
+            carreras tecnológicas. Mediante eventos, talleres y recursos,
+            trabajamos unidas para generar un impacto duradero en la industria
+            de la tecnología.
           </p>
           <p className="description text-right">
-            En <span>FemCoders Club </span>nos enfocamos en impulsar la inclusión, equidad y visibilidad de las mujeres en el sector de TI. Creemos en la diversidad y en la fuerza colectiva para superar obstáculos y lograr un impacto positivo en la industria tecnológica. Promovemos la colaboración y el apoyo mutuo, ofreciendo una plataforma donde las mujeres pueden compartir sus conocimientos, experiencias y recursos. <strong>Juntas, estamos construyendo un futuro más inclusivo y equitativo en el mundo de la tecnología.</strong>
+            En <span>FemCoders Club </span>nos enfocamos en impulsar la
+            inclusión, equidad y visibilidad de las mujeres en el sector de TI.
+            Creemos en la diversidad y en la fuerza colectiva para superar
+            obstáculos y lograr un impacto positivo en la industria tecnológica.
+            Promovemos la colaboración y el apoyo mutuo, ofreciendo una
+            plataforma donde las mujeres pueden compartir sus conocimientos,
+            experiencias y recursos.{" "}
+            <strong>
+              Juntas, estamos construyendo un futuro más inclusivo y equitativo
+              en el mundo de la tecnología.
+            </strong>
           </p>
         </div>
       </section>
@@ -37,9 +65,22 @@ const AboutPage: React.FC = () => {
                 <h3>Misión</h3>
               </div>
               <div className="flip-card-back">
-                <img src={FemCodersClubLogo} alt='femCoders Club logo' />
+                <img src={FemCodersClubLogo} alt="femCoders Club logo" />
                 <p>
-                  <strong>Nuestra misión es empoderar y elevar a las mujeres en el campo del desarrollo web.</strong> Trabajamos intensamente para cerrar la brecha de género en la tecnología, fomentando una comunidad que fortalece habilidades, conocimientos y confianza. Nos esforzamos incansablemente por tener un impacto positivo tanto en el sector tecnológico como en el desarrollo personal de nuestras miembros, a través de <strong>eventos, talleres y recursos que promueven la inclusión, la equidad y la diversidad.</strong>
+                  <strong>
+                    Nuestra misión es empoderar y elevar a las mujeres en el
+                    campo del desarrollo web.
+                  </strong>{" "}
+                  Trabajamos intensamente para cerrar la brecha de género en la
+                  tecnología, fomentando una comunidad que fortalece
+                  habilidades, conocimientos y confianza. Nos esforzamos
+                  incansablemente por tener un impacto positivo tanto en el
+                  sector tecnológico como en el desarrollo personal de nuestras
+                  miembros, a través de{" "}
+                  <strong>
+                    eventos, talleres y recursos que promueven la inclusión, la
+                    equidad y la diversidad.
+                  </strong>
                 </p>
               </div>
             </div>
@@ -51,9 +92,22 @@ const AboutPage: React.FC = () => {
                 <h3>Visión</h3>
               </div>
               <div className="flip-card-back">
-                <img src={FemCodersClubLogo} alt='logo femCoders Club' />
+                <img src={FemCodersClubLogo} alt="logo femCoders Club" />
                 <p>
-                  <strong>Nuestra visión es un futuro donde las mujeres desarrolladoras web lideren, innoven y den forma al panorama digital.</strong> Aspiramos a un sector IT equitativo e inclusivo, donde la diversidad y la inclusión sean principios fundamentales que impulsen el progreso tecnológico. Nuestro club aspira a ser el catalizador del cambio, donde cada mujer, sin importar su origen, encuentre estímulo, recursos y <strong>una comunidad vibrante para destacarse como codificadora y líder.</strong>
+                  <strong>
+                    Nuestra visión es un futuro donde las mujeres
+                    desarrolladoras web lideren, innoven y den forma al panorama
+                    digital.
+                  </strong>{" "}
+                  Aspiramos a un sector IT equitativo e inclusivo, donde la
+                  diversidad y la inclusión sean principios fundamentales que
+                  impulsen el progreso tecnológico. Nuestro club aspira a ser el
+                  catalizador del cambio, donde cada mujer, sin importar su
+                  origen, encuentre estímulo, recursos y{" "}
+                  <strong>
+                    una comunidad vibrante para destacarse como codificadora y
+                    líder.
+                  </strong>
                 </p>
               </div>
             </div>
@@ -63,31 +117,39 @@ const AboutPage: React.FC = () => {
       <section className="parallax bg3 full-height">
         <div className="bg3-content">
           <div className="values-section">
-          <h3 >Nuestros Valores</h3>
+            <h3>Nuestros Valores</h3>
 
             <CarouselValues />
           </div>
           <div className="commitment-section">
-            <div className='compromise-text'>
-              <img src={iconCommitment} alt="Icono Compromiso" width={33}/>
+            <div className="compromise-text">
+              <img src={iconCommitment} alt="Icono Compromiso" width={33} />
               <h3>Nuestro Compromiso</h3>
-              <p className='mx-8 mb-16 text-bodyText text-base text-contrast text-justify md:w-[550px]'>
-                Nuestra comunidad se compromete a realizar un esfuerzo real por disminuir la brecha de género digital, a fomentar la inclusión y a generar oportunidades para que las mujeres encuentren su lugar en la industria tecnológica.
-                Nos comprometemos a ser el espacio en el cual las mujeres encuentren motivación y recursos que les permitan crecer personal y profesionalmente, donde puedan y elijan crear comunidad para potenciar todo tipo de crecimiento.
+              <p className="mx-8 mb-16 text-bodyText text-base text-contrast text-justify md:w-[550px]">
+                Nuestra comunidad se compromete a realizar un esfuerzo real por
+                disminuir la brecha de género digital, a fomentar la inclusión y
+                a generar oportunidades para que las mujeres encuentren su lugar
+                en la industria tecnológica. Nos comprometemos a ser el espacio
+                en el cual las mujeres encuentren motivación y recursos que les
+                permitan crecer personal y profesionalmente, donde puedan y
+                elijan crear comunidad para potenciar todo tipo de crecimiento.
               </p>
-            </div >
-            <div className='compromise-img'>
-              <img src={imgCommitment} className="px-8 lg:order-1 lg:h-76 mb-8" alt="Icono de equidad" />
+            </div>
+            <div className="compromise-img">
+              <img
+                src={imgCommitment}
+                className="px-8 lg:order-1 lg:h-76 mb-8"
+                alt="Icono de equidad"
+              />
             </div>
           </div>
         </div>
       </section>
       <section className="parallax bg4 full-height">
-        <h3>Nuestras Ideas</h3>
+        <Collapse />
       </section>
     </>
   );
 };
 
 export default AboutPage;
-
