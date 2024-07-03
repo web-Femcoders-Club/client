988dfcc38a5e90d04492a4a5d1039bdf9f2cb260
Irina Ichim <onlyirina7@gmail.com>
2024-07-03 02:21:12 +0200

    Added styles for team page

--- src/features/Team/components/CardTeamMember.tsx
+++ src/features/Team/components/CardTeamMember.tsx
@@ -1,40 +1,81 @@
 import { useQuery } from "@tanstack/react-query";
+import { FaLinkedin, FaUser } from "react-icons/fa";
+import { PiFileMagnifyingGlassBold } from "react-icons/pi";
 import { getMember } from "../../../api/memberApi";
 import { Member } from "../../../types/types";
+import { HiUserGroup } from "react-icons/hi";
+import { useState } from "react";
+import "../../Team/page/CardTeamMember.css"; 
 
 const CardTeamMember = () => {
-  const { data, error, isLoading } = useQuery<Member[]>({
+  const { data } = useQuery<Member[]>({
     queryKey: ['members'],
     queryFn: getMember
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const [selectedMember, setSelectedMember] = useState<Member | null>(null);
 
-  if (error) {
-    return <div>Error loading members</div>;
-  }
+  const handleClick = (member: Member) => {
+    setSelectedMember(member); 
+    document.body.style.overflow = 'hidden';
+  };
 
-  if (!Array.isArray(data)) {
-    return <div>No members found</div>;
-  }
+  const handleCloseModal = () => {
+    setSelectedMember(null);
+    document.body.style.overflow = 'auto';
+  };
 
   return (
-    <div>
-      {data.map((member) => (
-        <div key={member.idMember}>
-          <h3>{member.memberName} {member.memberLastName}</h3>
-          <p>{member.memberRole}</p>
-          <img src={member.memberImage} alt={`${member.memberName} ${member.memberLastName}`} />
-          <a href={member.memberLinkedin}>LinkedIn</a>
+    <div className="card-team-member-container">
+      {data?.map((member) => (
+        <div key={member.idMember} className="card-team-member">
+          <div className="card-team-member-content">
+            <div className="card-team-member-image">
+              <img src={member.memberImage} alt="member image" className="member-image" />
+            </div>
+            <div className="card-team-member-details">
+              <h5 className="card-team-member-title"><FaUser className="icon" />{member.memberName} {member.memberLastName}</h5>
+              <p className="card-team-member-role"><HiUserGroup className="icon" />{member.memberRole}</p>
+              <p className="card-team-member-linkedin" title={member.memberLinkedin}>
+                <FaLinkedin className="icon" />
+                <a href={member.memberLinkedin} target="_blank" rel="noopener noreferrer" className="linkedin-url">
+                  {member.memberLinkedin}
+                </a>
+              </p>
+              <p className="card-team-member-description"><PiFileMagnifyingGlassBold className="icon" /><strong>{member.memberDescription.slice(0, 52)}...</strong></p>
+            </div>
+          </div>
+          <button className="secondary-button" onClick={() => handleClick(member)}>Ver más</button>
         </div>
       ))}
+      {selectedMember && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <div className="modal-header">
+              <img src={selectedMember.memberImage} alt="member image" className="modal-image" />
+              <h5 className="modal-title">{selectedMember.memberName} {selectedMember.memberLastName}</h5>
+              <p className="modal-role">| {selectedMember.memberRole} |</p>
+              <a href={selectedMember.memberLinkedin} className="modal-linkedin">{selectedMember.memberLinkedin}</a>
+            </div>
+            <div className="modal-body">
+              <p>{selectedMember.memberDescription}</p>
+            </div>
+            <div className="modal-footer">
+              <button className= 'primary-button' onClick={handleCloseModal}>Cerrar</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default CardTeamMember;
+
+
+
+
+
 
 
 
--- src/features/Team/page/TeamPage.tsx
+++ src/features/Team/page/TeamPage.tsx
@@ -1,29 +1,29 @@
-// src/features/Team/page/TeamPage.tsx
-
-import bgTeam from '../../../../public/bgTeamP1.png';
 import { styles } from '../../../style';
 import CardTeamMember from '../components/CardTeamMember';
 
+const GradientOverlay = ({ height }: { height: string }) => (
+  <div className={`absolute w-full h-${height} bottom-0 bg-gradient-to-t from-primary to-transparent`} />
+);
+
 const TeamPage = () => {
   return (
-    <div>
-      <section className="flex px-5 py-[100px] lg:py-[200px] xl:py-[250px] justify-center text-center h-fit relative" style={{ backgroundImage: `url(${bgTeam})`, backgroundSize: 'cover', backgroundRepeat: "no-repeat", backgroundPosition: "center top" }}>
-        <div className='absolute w-full h-[27%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary/40 to-transparent'/>
-        <div className='absolute w-full h-[25%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary/50 to-transparent'/>
-        <div className='absolute w-full h-[22%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary/60 to-transparent'/>
-        <div className='absolute w-full h-[20%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary/80 to-transparent'/>
-        <div className='absolute w-full h-[18%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary/90 to-transparent'/>
-        <div className='absolute w-full h-[10%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary to-transparent'/>
-        <div className='absolute w-full h-[10%] block xxl:hidden bottom-0 bg-gradient-to-t from-primary to-transparent'/>
-        <div className='absolute w-full h-[5%] xxl:block hidden bottom-0 bg-gradient-to-t from-primary to-transparent'/>
-        <h1 className={`${styles.heading3} font-headerText `}>
-          Conoce nuestro equipo
-        </h1> 
+    <div className="relative" style={{ backgroundImage: 'url("/bg1.png")', backgroundSize: 'cover', backgroundRepeat: "no-repeat", backgroundPosition: "center top" }}>
+      <section className="relative flex justify-center text-center h-fit py-[100px] lg:py-[200px] xl:py-[250px] px-5">
+        <GradientOverlay height="27%" />
+        <GradientOverlay height="25%" />
+        <GradientOverlay height="22%" />
+        <GradientOverlay height="20%" />
+        <GradientOverlay height="18%" />
+        <GradientOverlay height="10%" />
+        <GradientOverlay height="5%" />
+        <h1 className={`${styles.heading3} font-headerText`}>Conoce nuestro equipo</h1>
       </section>
       <section className='w-full flex flex-col items-center py-10 lg:py-16 gap-10 xl:gap-20'>
-        <p className="text-xl text-contrast font-semibold font-headerText max-w-[1200px] text-center md:mx-10 lg:mx-20 sm:mx-10 mx-5">Somos una vibrante comunidad de mujeres apasionadas por la tecnología, el crecimiento personal y el empoderamiento femenino.
-          En FemCoders Club, nos dedicamos a crear un espacio seguro y enriquecedor donde mujeres tecnólogas puedan reunirse, colaborar y crecer juntas. Creemos firmemente en el poder de la diversidad y la fuerza colectiva para superar barreras y lograr un impacto significativo en la industria tecnológica.</p>
-       <CardTeamMember />
+        <h4 className="max-w-4xl text-center mx-4" style={{ color: '#4737bb', marginTop: '1rem' }}>
+          Somos una vibrante comunidad de mujeres apasionadas por la tecnología, el crecimiento personal y el empoderamiento femenino.
+          En FemCoders Club, nos dedicamos a crear un espacio seguro y enriquecedor donde mujeres tecnólogas puedan reunirse, colaborar y crecer juntas. Creemos firmemente en el poder de la diversidad y la fuerza colectiva para superar barreras y lograr un impacto significativo en la industria tecnológica.
+        </h4>
+        <CardTeamMember />
       </section>
     </div>
   );
