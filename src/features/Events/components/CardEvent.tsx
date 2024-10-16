b4f1ad1a4659f8c9c215f57c19afdb2f24529b82
Irina Ichim <onlyirina7@gmail.com>
2024-10-16 14:51:51 +0200

    Update .gitignore and modify CardEvent and EventsPage components

--- src/features/Events/components/CardEvent.tsx
+++ src/features/Events/components/CardEvent.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import imgEvents from '/apoyomujeres.png';
-import iconDate from '/iconDate.png';
-import iconLocation from '/iconLocation.png';
-
+import React from "react";
+import imgEvents from "/apoyomujeres.png";
+import iconDate from "/iconDate.png";
+import iconLocation from "/iconLocation.png";
 
 interface CardEventProps {
   title: string;
@@ -11,30 +10,57 @@ interface CardEventProps {
   location: string;
   description: string;
   eventUrl: string;
+  start: {
+    local: string;
+  };
 }
 
-const CardEvent: React.FC<CardEventProps> = ({ title, image, date, location, description, eventUrl }) => {
+const CardEvent: React.FC<CardEventProps> = ({
+  title,
+  image,
+  date,
+  location,
+  description,
+  eventUrl,
+  start,
+}) => {
+  const isPastEvent = new Date(start.local) < new Date();
+
   return (
     <div className="event-card-custom">
       <div className="event-flip-card-inner-custom">
         <div className="event-flip-card-front-custom">
           <h2 className="event-card-title-custom">{title}</h2>
-          <img className="event-card-image-custom" src={image || imgEvents} alt={title} />
+          <img
+            className="event-card-image-custom"
+            src={image || imgEvents}
+            alt={title}
+          />
         </div>
         <div className="event-flip-card-back-custom">
           <div className="event-card-body-custom">
             <div className="flex items-center gap-2 mb-2">
-              <img src={iconDate} alt="Icon date" className="opacity-70 h-4 icon-orange" />
+              <img
+                src={iconDate}
+                alt="Icon date"
+                className="opacity-70 h-4 icon-orange"
+              />
               <p className="text-bodyText text-contrast/80">{date}</p>
             </div>
             <div className="flex items-center gap-2">
-              <img src={iconLocation} alt="Icon location" className="opacity-70 h-4 icon-orange" />
+              <img
+                src={iconLocation}
+                alt="Icon location"
+                className="opacity-70 h-4 icon-orange"
+              />
               <p className="text-bodyText text-contrast/80">{location}</p>
             </div>
             <p className="text-bodyText text-contrast">{description}</p>
-            <a href={eventUrl} target="_blank" rel="noopener noreferrer">
-              <button className="tertiary-button">Adquiere tu entrada</button>
-            </a>
+            {!isPastEvent && (
+              <a href={eventUrl} target="_blank" rel="noopener noreferrer">
+                <button className="tertiary-button">Adquiere tu entrada</button>
+              </a>
+            )}
           </div>
         </div>
       </div>
@@ -43,6 +69,3 @@ const CardEvent: React.FC<CardEventProps> = ({ title, image, date, location, des
 };
 
 export default CardEvent;
-
-
-
--- src/features/Events/page/EventsPage.tsx
+++ src/features/Events/page/EventsPage.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getPastEvents, getUpcomingEvents } from '../../../api/eventsApi';
-import FemSpinner from '../../../components/Spinner';
-import CardEvent from '../components/CardEvent';
-import CustomCarousel from '../components/CustomCarousel';
-import './EventsPage.css';
-import textofemcoders from '/textofemcodersclub.png';
-import bg4 from '/bg4.png';
-import { Helmet } from 'react-helmet';
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getPastEvents, getUpcomingEvents } from "../../../api/eventsApi";
+import FemSpinner from "../../../components/Spinner";
+import CardEvent from "../components/CardEvent";
+import CustomCarousel from "../components/CustomCarousel";
+import "./EventsPage.css";
+import textofemcoders from "/textofemcodersclub.png";
+import bg4 from "/bg4.png";
+import { Helmet } from "react-helmet";
 import "./../../Home/page/Home.css";
 
 interface Event {
@@ -38,13 +38,21 @@ const EventsPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const eventsPerPage = 3;
 
-  const { data: pastEventsData, isLoading: isLoadingPastEvents, error: pastEventsError } = useQuery({
-    queryKey: ['pastEvents'],
+  const {
+    data: pastEventsData,
+    isLoading: isLoadingPastEvents,
+    error: pastEventsError,
+  } = useQuery({
+    queryKey: ["pastEvents"],
     queryFn: getPastEvents,
   });
 
-  const { data: upcomingEventsData, isLoading: isLoadingUpcomingEvents, error: upcomingEventsError } = useQuery({
-    queryKey: ['upcomingEvents'],
+  const {
+    data: upcomingEventsData,
+    isLoading: isLoadingUpcomingEvents,
+    error: upcomingEventsError,
+  } = useQuery({
+    queryKey: ["upcomingEvents"],
     queryFn: getUpcomingEvents,
   });
 
@@ -57,88 +65,141 @@ const EventsPage = () => {
     currentPage * eventsPerPage
   );
 
-  const totalPages = Math.ceil((pastEventsData?.events.length || 0) / eventsPerPage);
+  const totalPages = Math.ceil(
+    (pastEventsData?.events.length || 0) / eventsPerPage
+  );
 
   return (
     <>
       <Helmet>
         <title>FemCoders Club Events</title>
-        <meta name="description" content="Upcoming and past events organized by FemCoders Club" />
-        <meta name="keywords" content="FemCoders, FemCoders Club, events, tech events, women in tech, coding, programming, technology, community" />
+        <meta
+          name="description"
+          content="Upcoming and past events organized by FemCoders Club"
+        />
+        <meta
+          name="keywords"
+          content="FemCoders, FemCoders Club, events, tech events, women in tech, coding, programming, technology, community"
+        />
       </Helmet>
-      
-      <section style={{ backgroundImage: `url(${textofemcoders})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center bottom' }} className="background-image-mobile">
+
+      <section
+        style={{
+          backgroundImage: `url(${textofemcoders})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center bottom",
+        }}
+        className="background-image-mobile"
+      >
         <h1 className="text-eventos">Próximos eventos</h1>
       </section>
 
       <section>
-        <div className='mt-16 flex items-center justify-center flex-col gap-y-8 p-5'>
+        <div className="mt-16 flex items-center justify-center flex-col gap-y-8 p-5">
           {isLoadingUpcomingEvents ? (
             <FemSpinner />
+          ) : upcomingEventsData?.events &&
+            upcomingEventsData.events.length > 0 ? (
+            upcomingEventsData.events.map((event: Event) => {
+              const date = new Date(event.start.local).toLocaleDateString(
+                "es-ES",
+                {
+                  weekday: "long",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                }
+              );
+              return (
+                <CardEvent
+                  key={event.id}
+                  title={event.name.text}
+                  image={event.logo?.original?.url || ""}
+                  date={date}
+                  location={
+                    event.venue?.address?.localized_address_display || ""
+                  }
+                  description={event.description.text}
+                  eventUrl={event.url}
+                  start={event.start}
+                />
+              );
+            })
           ) : (
-            upcomingEventsData?.events && upcomingEventsData.events.length > 0 ? (
-              upcomingEventsData.events.map((event: Event) => {
-                const date = new Date(event.start.local).toLocaleDateString('es-ES', { weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
-                return (
-                  <CardEvent
-                    key={event.id}
-                    title={event.name.text}
-                    image={event.logo?.original?.url || ''}
-                    date={date}
-                    location={event.venue?.address?.localized_address_display || ''}
-                    description={event.description.text}
-                    eventUrl={event.url}
-                  />
-                );
-              })
-            ) : (
-              <p>No hay próximos eventos disponibles</p>
-            )
+            <p>No hay próximos eventos disponibles</p>
           )}
         </div>
       </section>
 
       <section className="parallax bg2 centered-section">
-        <h3 className="carousel-heading">Gracias a todas las ponentes que empoderan a nuestra comunidad</h3>
+        <h3 className="carousel-heading">
+          Gracias a todas las ponentes que empoderan a nuestra comunidad
+        </h3>
         <p className="carousel-subheading">
-          Con la ayuda de estas ponentes, nuestra comunidad adquiere conocimiento, empoderamiento y conexión.
+          {" "}
+          Gracias a la colaboración de estas ponentes, nuestra comunidad se
+          fortalece a través del conocimiento, el empoderamiento y la conexión.{" "}
         </p>
         <CustomCarousel />
       </section>
 
-      <section className="pt-8 p-5" style={{ backgroundImage: `url(${bg4})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-        <h1 className="text-3xl font-bold text-secondary flex justify-center text-center mb-8">Eventos Pasados</h1>
-        <div className='flex items-center justify-center flex-col gap-y-8'>
+      <section
+        className="pt-8 p-5"
+        style={{
+          backgroundImage: `url(${bg4})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <h1 className="text-3xl font-bold text-secondary flex justify-center text-center mb-8">
+          Eventos Pasados
+        </h1>
+        <div className="flex items-center justify-center flex-col gap-y-8">
           {isLoadingPastEvents ? (
             <FemSpinner />
+          ) : paginatedEvents && paginatedEvents.length > 0 ? (
+            paginatedEvents.map((event: Event) => {
+              const date = new Date(event.start.local).toLocaleDateString(
+                "es-ES",
+                {
+                  weekday: "long",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                }
+              );
+              return (
+                <CardEvent
+                  key={event.id}
+                  title={event.name.text}
+                  image={event.logo?.original?.url || ""}
+                  date={date}
+                  location={
+                    event.venue?.address?.localized_address_display || ""
+                  }
+                  description={event.description.text}
+                  eventUrl={event.url}
+                  start={event.start}
+                />
+              );
+            })
           ) : (
-            paginatedEvents && paginatedEvents.length > 0 ? (
-              paginatedEvents.map((event: Event) => {
-                const date = new Date(event.start.local).toLocaleDateString("es-ES", { weekday: "long", month: "long", day: "numeric", hour: "numeric", minute: "numeric", hour12: true });
-                return (
-                  <CardEvent
-                    key={event.id}
-                    title={event.name.text}
-                    image={event.logo?.original?.url || ''}
-                    date={date}
-                    location={event.venue?.address?.localized_address_display || ''}
-                    description={event.description.text}
-                    eventUrl={event.url}
-                  />
-                );
-              })
-            ) : (
-              <p>No hay eventos pasados disponibles</p>
-            )
+            <p>No hay eventos pasados disponibles</p>
           )}
         </div>
-        
+
         <div className="flex justify-center mt-4">
           <div className="btn-group pagination-custom">
             {Array.from({ length: totalPages }, (_, index) => (
               <button
                 key={index}
-                className={`btn ${currentPage === index + 1 ? 'btn-active' : ''}`}
+                className={`btn ${
+                  currentPage === index + 1 ? "btn-active" : ""
+                }`}
                 onClick={() => setCurrentPage(index + 1)}
               >
                 {index + 1}
@@ -149,8 +210,6 @@ const EventsPage = () => {
       </section>
     </>
   );
-}
+};
 
 export default EventsPage;
-
-
