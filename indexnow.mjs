1de7d2eefdf92c96d7ca7f98f403bec8e9cbcf49
Irina Ichim <onlyirina7@gmail.com>
2025-07-21 21:05:27 +0200

    update styles categorry buttons

---
+++ indexnow.mjs
@@ -0,0 +1,36 @@
+// indexnow.mjs
+import https from 'https';
+
+const data = JSON.stringify({
+  host: 'www.femcodersclub.com',
+  key: '6befac582d614d1bb40630f1ea6aeca2',
+  keyLocation: 'https://www.femcodersclub.com/6befac582d614d1bb40630f1ea6aeca2.txt',
+  urlList: [
+    'https://www.femcodersclub.com/',
+    'https://www.femcodersclub.com/eventos',
+    'https://www.femcodersclub.com/equipo',
+    'https://www.femcodersclub.com/blog',
+    'https://www.femcodersclub.com/contacto',
+    'https://www.femcodersclub.com/femcoders-quienes-somos',
+    'https://www.femcodersclub.com/login'
+  ]
+});
+
+const options = {
+  hostname: 'api.indexnow.org',
+  path: '/indexnow',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Length': data.length
+  }
+};
+
+const req = https.request(options, res => {
+  console.log(`✅ Status: ${res.statusCode}`);
+  res.on('data', d => process.stdout.write(d));
+});
+
+req.on('error', error => console.error('❌ Error:', error));
+req.write(data);
+req.end();
